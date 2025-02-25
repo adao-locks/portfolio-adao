ae03d2a3dc224a485eb712795d867d648561eb17
Eduardo Adão Locks <122515706+adao-locks@users.noreply.github.com>
2025-02-25 19:53:26 -0300

    ajusted icons in skillstree, ajusted contact component and updated skillstree content

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // ✅ IMPORTADO
 
 @Component({
-  selector: 'app-contact',
+  selector: 'app-contato',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContatoComponent {
+  name: string = '';
+  email: string = '';
+  message: string = '';
 
+  enviarFormulario() {
+    if (this.name && this.email && this.message) {
+      console.log('Formulário enviado com sucesso!', {
+        nome: this.name,
+        email: this.email,
+        mensagem: this.message
+      });
+
+      alert('Mensagem enviada com sucesso!');
+      this.name = '';
+      this.email = '';
+      this.message = '';
+    } else {
+      alert('Por favor, preencha todos os campos.');
+    }
+  }
 }
